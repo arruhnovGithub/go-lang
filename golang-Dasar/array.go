9a7ae5b533adc730fa7535e5d30d57c42fff2f08
Arief Noviyanto <58097100+arruhnovGithub@users.noreply.github.com>
2022-01-02 22:02:46 +0700

    Update array.go

--- golang-Dasar/array.go
+++ golang-Dasar/array.go
@@ -11,9 +11,25 @@ func main() {
 	// names[3] = "law"
 
 	// fmt.Println(names[0], names[1], names[2], names[3])
+	// var fruits = [4]string{"apple", "grape", "banana", "melon"}
 
-	var fruits = [4]string{"apple", "grape", "banana", "melon"}
+	// fmt.Println("Jumlah element \t\t", len(fruits))
+	// fmt.Println("Isi semua element \t", fruits)
 
-	fmt.Println("Jumlah element \t\t", len(fruits))
-	fmt.Println("Isi semua element \t", fruits)
+	// var fruits [4]string
+
+	// // cara horizontal
+	// fruits = [4]string{"apple", "grape", "banana", "melon"}
+
+	// // cara vertikal
+	// fruits = [4]string{
+	// 	"apple",
+	// 	"grape",
+	// 	"banana",
+	// 	"melon",
+	// }
+	var numbers = [...]int{2, 3, 2, 4, 3}
+
+	fmt.Println("data array \t:", numbers)
+	fmt.Println("jumlah elemen \t:", len(numbers))
 }
